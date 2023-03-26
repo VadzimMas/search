af930e804280b86f3b0baa5a59441f6da040ed9d
VM-Web <mas.usa.gov@gmail.com>
2023-03-25 22:29:57 -0400

    app.module.scss

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,17 @@
-import AppStyled from './App.styled'
 import Sidebar from './components/sidebar/Sidebar'
 import {Outlet} from 'react-router-dom'
 import ScrollToTop from '../hooks/ScrollToTop'
 import {Fragment} from 'react'
+import s from './app.module.scss'
 
 function App() {
   return (
     <Fragment>
-      <ScrollToTop/>
-      <AppStyled>
-        <Sidebar/>
-        <div className="detail" id="detail"><Outlet/></div>
-      </AppStyled>
+      <ScrollToTop />
+      <div className={s.app}>
+        <Sidebar />
+        <div className={s.detail} id="detail"><Outlet /></div>
+      </div>
     </Fragment>
   )
 }
