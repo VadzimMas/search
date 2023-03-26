03fd7867babfc049715106ff269429ba742d5ebe
VM-Web <mas.usa.gov@gmail.com>
2023-03-25 23:05:37 -0400

    spiner

--- src/features/crownClosing/components/shop/products-list/products-item/Products-item.jsx
+++ src/features/crownClosing/components/shop/products-list/products-item/Products-item.jsx
@@ -1,30 +1,30 @@
 import ProductCard from '../../../product-card/Product-card'
 import {useNavigate} from 'react-router-dom'
-import ProductsItemStyled from './Products-item-styled'
-
+import s from './products-item.module.scss'
 
 function ProductsItem({title, products}) {
   const navigate = useNavigate()
   const navigateToCategory = () => navigate(title)
   const renderedItem = () => {
     if (products) {
-      return products.items.filter((_, index) => index < 4).map((product) => {
-        return <ProductCard key={product.id} product={product}/>
-      })
+      return products.items.filter((_, index) => index < 4)
+                     .map((product) => {
+                       return <ProductCard key={product.id} product={product} />
+                     })
     } else {
       return null
     }
   }
   
   return (
-    <ProductsItemStyled>
-      <h2 className="title" onClick={navigateToCategory}>
+    <div className={s.productsItem}>
+      <h2 className={s.title} onClick={navigateToCategory}>
         {title.toUpperCase()}
       </h2>
-      <div className="category">
+      <div className={s.category}>
         {renderedItem()}
       </div>
-    </ProductsItemStyled>
+    </div>
   )
 }
 
