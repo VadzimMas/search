415afc302b33c651857aea5dac32fd628e47f84b
VM-Web <mas.usa.gov@gmail.com>
2023-04-02 17:25:21 -0400

    finish fake payment

--- src/features/crownClosing/components/checkout/fakePament/FakePayment.jsx
+++ src/features/crownClosing/components/checkout/fakePament/FakePayment.jsx
@@ -3,26 +3,38 @@ import EMVChip from '../../../assets/EMVChip.svg'
 import Meastro from '../../../assets/Meastro.svg'
 import {useFetchUserQuery} from '../../../store/api/user.api'
 import {faker} from '@faker-js/faker'
-
+import {useNavigate} from 'react-router-dom'
+import { CgSmartphoneChip } from 'react-icons/cg';
 
 function FakePayment() {
-  
+  const navigate = useNavigate()
   const {data} = useFetchUserQuery()
+  
+  const handlePayment = () => {
+    navigate('/crownClothing/order')
+  }
   
   return (
     <div className={s.fakePayment}>
-      <img src={faker.image.unsplash.nature()} alt="" />
-      <img className={s.chip} src={EMVChip} alt="" />
+      <img className={s.img} src={faker.image.unsplash.nature(300,200)} alt="" />
+      <div className={s.bgTon}></div>
+      <CgSmartphoneChip className={s.chip}/>
       <img className={s.logo} src={Meastro} alt="" />
       
-      <div className={s.fullName}></div>
       <div className={s.fields}>
-        <div>
-          <span className={s.fullName}>{data?.displayName}</span>
-          <span className={s.expiration}>04/30</span>
-        </div>
-        <div>1234 4567 8910 9876</div>
+          <div className={s.bankNmae}>{faker.commerce.productName()+" Bank"}</div>
+          <div className={s.fieldsContainer}>
+            <div className={s.name}>
+               <span className={s.fullName}>{data?.displayName}</span>
+               <span className={s.expiration}>exp: 04/30</span>
+            </div>
+            <div className={s.cardNumber}>
+              <span>1234 4567 8910 9876</span>
+              <span>security code: 482</span>
+            </div>
+          </div>
       </div>
+      <button className={`${s.btn} google`} onClick={handlePayment}>Order</button>
     </div>
   )
 }
--- src/router/router.js
+++ src/router/router.js
@@ -10,59 +10,7 @@ import Checkout from '../features/crownClosing/components/checkout/Checkout'
 import ProductsList from '../features/crownClosing/components/shop/products-list/Products-list'
 import Category from '../features/crownClosing/components/category/Category'
 import Index from '../features/crownClosing/Index'
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App/>,
-//     errorElement: <ErrorPage/>,
-//     children: [
-//       {
-//         index: true,
-//         element: <Home/>,
-//       },
-//       {
-//         path: 'searchUsers',
-//         element: <SearchUsers/>,
-//       },
-//       // Crown Clothing
-//       {
-//         path: 'crownClothing',
-//         element: <CrownClothing/>,
-//         children: [
-//           {
-//             index: true,
-//             element: <DirectoryList/>,
-//           },
-//           {
-//             path: 'shop',
-//             element: <Shop/>,
-//             children: [
-//               {
-//                 index: true,
-//                 element: <ProductsList/>,
-//               },
-//               {
-//                 path: ':id',
-//                 element: <Category/>,
-//               },
-//             ],
-//           },
-//           {
-//             path: 'auth',
-//             element: <Authentication/>,
-//           },
-//           {
-//             path: 'checkout',
-//             element: <Checkout/>,
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ])
-
+import Order from '../features/crownClosing/components/checkout/order/Order'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -77,6 +25,7 @@ const router = createBrowserRouter(
         </Route>
         <Route path="auth" element={<Authentication/>}/>
         <Route path="checkout" element={<Checkout/>}/>
+        <Route path="order" element={<Order/>}/>
       </Route>
     </Route>
   ))
