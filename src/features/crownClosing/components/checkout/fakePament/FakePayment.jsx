02c5a0709fe79a9829ad7a277203827b4e92dae9
VM-Web <mas.usa.gov@gmail.com>
2023-04-02 14:35:12 -0400

    start fake payment

---
+++ src/features/crownClosing/components/checkout/fakePament/FakePayment.jsx
@@ -0,0 +1,12 @@
+import s from './fakePayment.module.scss'
+
+
+function FakePayment() {
+  return (
+    <div className={s.fakePayment}>
+                                   FakePayment
+    </div>
+  )
+}
+
+export default FakePayment
